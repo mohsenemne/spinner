fac4de6cdf9e50a610b598d0d68601c13b693805
mohsenemne <mohsenemne@yahoo.com>
2019-03-10 21:32:35 +0330

    ball created

--- app/src/main/java/com/cps/spinner/Gravity.java
+++ app/src/main/java/com/cps/spinner/Gravity.java
@@ -2,6 +2,7 @@ package com.cps.spinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Gravity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class Gravity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gravity);
+
+        ImageView ball = findViewById(R.id.ball);
+        ball.setY(100);
+        ball.setX(200);
     }
+
+
 }
--- app/src/main/java/com/cps/spinner/Gyroscope.java
+++ app/src/main/java/com/cps/spinner/Gyroscope.java
@@ -2,6 +2,7 @@ package com.cps.spinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Gyroscope extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class Gyroscope extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gyroscope);
+
+        ImageView ball = findViewById(R.id.ball);
+        ball.setY(100);
+        ball.setX(200);
     }
 }
