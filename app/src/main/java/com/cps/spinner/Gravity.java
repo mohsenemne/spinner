1105e994ecb3eb05cee14287d61f1e02be67f2f3
mohsenemne <mohsenemne@yahoo.com>
2019-03-11 00:05:03 +0330

    ball moves

--- app/src/main/java/com/cps/spinner/Gravity.java
+++ app/src/main/java/com/cps/spinner/Gravity.java
@@ -1,20 +1,66 @@
 package com.cps.spinner;
 
+import android.graphics.Point;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 public class Gravity extends AppCompatActivity {
+
+    private int screenWidth;
+    private int screenHeight;
+
+    private ImageView ball;
+
+    private float ballX;
+    private float ballY;
+
+    private Handler handler = new Handler();
+    private Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gravity);
 
-        ImageView ball = findViewById(R.id.ball);
-        ball.setY(100);
-        ball.setX(200);
+        ball = findViewById(R.id.ball);
+
+        WindowManager wm = getWindowManager();
+        Display disp = wm.getDefaultDisplay();
+        Point size = new Point();
+        disp.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+        ball.setX(0);
+        ball.setY(0);
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        changePos();
+                    }
+                });
+            }
+        }, 0,20);
     }
 
+
+    public void changePos(){
+        ballX += 10;
+        ballY += 10;
+        ball.setX(ballX);
+        ball.setY(ballY);
+    }
 
 }
