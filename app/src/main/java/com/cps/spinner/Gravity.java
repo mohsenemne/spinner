faafe4c34506ff0bf000d5e692039d43008317a6
mohsenemne <mohsenemne@yahoo.com>
2019-03-11 00:45:08 +0330

    gravity finished, not tested

--- app/src/main/java/com/cps/spinner/Gravity.java
+++ app/src/main/java/com/cps/spinner/Gravity.java
@@ -1,6 +1,11 @@
 package com.cps.spinner;
 
+import android.content.Context;
 import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,23 +17,31 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
-public class Gravity extends AppCompatActivity {
+public class Gravity extends AppCompatActivity implements SensorEventListener{
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
 
     private int screenWidth;
     private int screenHeight;
 
     private ImageView ball;
 
-    private float ballX;
-    private float ballY;
+    private float ballX = 0;
+    private float ballY = 0;
 
-    private Handler handler = new Handler();
-    private Timer timer = new Timer();
+    private float ballVX = 0;
+    private float ballVY = 0;
+
+    private float timestamp = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gravity);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
 
         ball = findViewById(R.id.ball);
 
@@ -42,25 +55,30 @@ public class Gravity extends AppCompatActivity {
         ball.setX(0);
         ball.setY(0);
 
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        changePos();
-                    }
-                });
-            }
-        }, 0,20);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME);
     }
 
 
-    public void changePos(){
-        ballX += 10;
-        ballY += 10;
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float ax = event.values[0];
+        float ay = event.values[1];
+
+        float dt = event.timestamp - timestamp;
+        timestamp = event.timestamp;
+
+        ballX += (ax * dt * dt / 2.) + ballVX * dt;
+        ballY += (ay * dt * dt / 2.) + ballVY * dt;
+
+        ballVX += ax * dt;
+        ballVY += ay * dt;
+
         ball.setX(ballX);
         ball.setY(ballY);
     }
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
